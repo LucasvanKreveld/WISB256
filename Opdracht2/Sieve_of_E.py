97f451bfd2e107b462d0ec763139d925644ab3e3
Lucas van Kreveld <lucasvankreveld@hotmail.com>
2015-05-07 13:10:37 +0000

    Snellere versie

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -7,20 +7,24 @@ T1=time.perf_counter()
 
 t=[]
 for i in range(2,N):
-    t += [str(i)]
+    t += [i]
     
-for p in range(2,N):
-    x = True
-    for m in range(2,int(p/2+1)):
-        if x == True and p%m==0:
-            t.remove(str(p))
-            x = False       #om p niet meerdere malen te verwijderen
+for k in t:
+    if k!=0:
+        for l in range(2,int(N/k)+1):
+            if 0<k*l<N:
+                t[k*l-2]=0
 
+u=[]
+for j in t:
+    if j!=0:
+        u.append(str(j))
+    
 T2=time.perf_counter()
 
-print('Found', len(t), 'Prime numbers smaller than', N, 'in', T2-T1, 'sec.')
+print('Found', len(u), 'Prime numbers smaller than', N, 'in', T2-T1, 'sec.')
 
 verzetten = open(sys.argv[2], 'w')
 
-for k in range(0,len(t)):
-    verzetten.write(t[k] + '\n')
+for k in range(0,len(u)):
+    verzetten.write(u[k] + '\n')
