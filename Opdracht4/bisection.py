525711f2c06f6d285a1e4aacfd63666c56cb97d2
Lucas van Kreveld <lucasvankreveld@hotmail.com>
2015-05-21 12:54:07 +0000

    Versie 3

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -8,15 +8,13 @@ def findRoot(f,a,b,epsilon):
         print('Check different interval')
         exit()
     
-    while abs(a-b)>=epsilon:
+    while abs(a-b)>epsilon:
         
         if f(a)==0:
-            print('x =', a)
-            exit()
+            return(a)
         
         elif f(b)==0:
-            print('x =', b)
-            exit()
+            return(b)
         
         elif f(a)<0 and f(b)>0:
             if f((a+b)/2)<0:
@@ -29,6 +27,6 @@ def findRoot(f,a,b,epsilon):
                 b = (a+b)/2
             else:
                 a = (a+b)/2
-    print((a+b)/2)
-        
+    return((a+b)/2)
+
 
